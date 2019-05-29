9d8514768ba1ea01cbfb6a5773c3d00c932dbe29
ramsatt <ramsatt@gmail.com>
2019-05-29 16:02:59 +0530

    Direction Between Two Location

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'simple', loadChildren: './simple/simple.module#SimplePageModule' },
+  { path: 'geolocation', loadChildren: './geolocation/geolocation.module#GeolocationPageModule' },
+  { path: 'marker', loadChildren: './marker/marker.module#MarkerPageModule' },
+  { path: 'direction', loadChildren: './direction/direction.module#DirectionPageModule' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,22 @@ export class AppComponent {
       icon: 'home'
     },
     {
-      title: 'List',
-      url: '/list',
+      title: 'Simple Map',
+      url: '/simple',
+      icon: 'list'
+    },
+    {
+      title: 'Geo Location',
+      url: '/geolocation',
+      icon: 'list'
+    },
+    {
+      title: 'Custom Marker',
+      url: '/marker',
+      icon: 'list'
+    }, {
+      title: 'Direction',
+      url: '/direction',
       icon: 'list'
     }
   ];
